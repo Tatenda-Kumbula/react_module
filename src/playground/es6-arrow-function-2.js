126b463b2020b050c024c34e3c3cb78f726dbcb7
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-26 20:52:10 +0200

    Add files via upload

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -11,3 +11,17 @@ const add = (a, b) => {
       return this.cities.map((city) => this.name + ' has lived in ' + city);
     }
   };
+
+
+  console.log(user.printPlacesLived());
+
+  const multiplier = {
+    numbers: [10, 20, 30],
+    multiplyBy: 3,
+    multiply() {
+      return this.numbers.map((number) => number * this.multiplyBy);
+    }
+  };
+  
+  console.log(multiplier.multiply());
+  
