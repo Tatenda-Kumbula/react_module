d3e326678b796af0df793335a62506527b97b8d3
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-26 20:51:20 +0200

    Add files via upload

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,30 +1,13 @@
-//src/playground/es6-arrow-function-2.js --out-file=public/scripts/app.js --presets=env,react --watch
-
-const add = function (a, b){
-   // console.log(arguments);
+const add = (a, b) => {
+ // console.log(arguments);
     return a + b;
-};
-console.log(add(55, 1, 1001));
-
-//this keyword
-const user = {
+  };
+  console.log(add(55, 1, 1001));
+  
+  const user = {
     name: 'Tatenda',
-    cities: ['London', 'New York', 'Toronto'],
-    printPlacesLived () {
-    return this.cities.map((city) => this.name + ' has lived in ' + city);
+    cities: ['Johannesburg', 'Cape Town', 'Dublin'],
+    printPlacesLived() {
+      return this.cities.map((city) => this.name + ' has lived in ' + city);
     }
-};
-
-console.log(user.printPlacesLived());
-
-
-const multiplier = {
-    numbers: [ 1, 2, 3],
-    multiplyBy: 2,
-    multiply() {   
-    return this.numbers.map((number) => number * this.multiplyBy);
-    
-}
-};
-
-console.log(multiplier.multiply());
+  };
