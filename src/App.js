c7ec5ba179218b11cc7bea31892d2d0e743decee
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-26 19:10:11 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ console.log('App.js is running!');
 
 var app= {
     title: 'Indecision App',
-    subtitle: 'Put ypur life in the hands of a computer'
+    subtitle: 'Put ypur life in the hands of a computer',
+    options: ['One', 'Two']
 };
 // JSX - JavaScript XML
 var template = (
   <div> 
     <h1>Indecision App.js!</h1>
     <p>This is some info</p>
+    <p>{app.options.length > 0 ? 'Here are your options': 'No options'}</p>
     <ol>
         <li>Item one</li>
         <li>Item two</li>
