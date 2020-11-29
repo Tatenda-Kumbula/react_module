d9a5fc0d9e5a6077bc1d95296c0e604ed0d1440f
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-29 08:08:21 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ console.log('App.js is running!');
 var app= {
     title: 'Indecision App',
     subtitle: 'Put ypur life in the hands of a computer',
-    options: ['One', 'Two']
+    options: []
 };
-// JSX - JavaScript XML
+
 var template = (
   <div> 
     <h1>Indecision App.js!</h1>
@@ -18,34 +18,5 @@ var template = (
   </div>
 );
 
-
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-};
-
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
-
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
