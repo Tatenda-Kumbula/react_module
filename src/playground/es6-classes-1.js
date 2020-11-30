8877ac47d71d233410bbcf7c4163e24ab93d3074
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-30 16:03:11 +0200

    Add files via upload

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -22,7 +22,42 @@ class Student extends Person {
          super(name, age);
          this.major = major;
      }
- }
+hasMajor() {
+    return !!this.major;
+}
+getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+        description += ` Their major is ${this.major}.`;
+      }
+  
+      return description;
+    }
+  }
+  
+  class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+      super(name, age);
+      this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+      let greeting = super.getGreeting();
+  
+      if (this.homeLocation) {
+        greeting += ` I am visiting from ${this.homeLocation}.`;
+      }
+  
+      return greeting;
+    }
+  }
+  
+  const me = new Traveler('Andrew Mead', 26, 'Philadelphia');
+  console.log(me.getGreeting());
+  
+  const other = new Traveler(undefined, undefined, 'Nowhere');
+  console.log(other.getGreeting());
+  
+
 
 const me = new Person('Tatenda Kumbula', 19);
 console.log(me.getDescription());
