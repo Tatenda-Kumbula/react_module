c8d3f24da3b5478a2453cccb68329b5369db87de
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-30 15:24:25 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -20,6 +20,38 @@ const onFormSubmit = (e) => {
     }
 };
 
+const onRemoveAll = () => {
+    app.options = [];
+    render();
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+    const template = (
+      <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <p>{app.options.length}</p>
+        <button onClick={onRemoveAll}>Remove All</button>
+        <ol>
+          <li>Item one</li>
+          <li>Item two</li>
+        </ol>
+        <form onSubmit={onFormSubmit}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  
+    ReactDOM.render(template, appRoot);
+  };
+  
+  render();
+  
+
 var template = (
   <div> 
     <h1>Indecision App.js!</h1>
