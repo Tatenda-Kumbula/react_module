de6add7385afc1a72904a2841bde5c2259b33a16
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-10 13:11:54 +0200

    Add files via upload

--- src/playground/components/Options.js
+++ src/playground/components/Options.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Option from './Option';
 
 const Options = (props) => (
- 
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove All</button>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your Options</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove All
+    </button>
+    </div>
+
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
     {
       props.options.map((option) => (
         <Option
@@ -15,10 +23,7 @@ const Options = (props) => (
         />
       ))
     }
-  
-    </div>
-
-    );
-
+  </div>
+);
 
 export default Options;
