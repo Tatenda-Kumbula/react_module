b70fd8dea1733f979e77b47716f4a8063f820d1e
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2021-01-19 06:51:36 +0200

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,30 @@ const path=require('path');
 
 
 module.exports = {
-  entry: './src/App.js ', 
+  entry: './src/src/App.js',
   output:{
       path:path.join(__dirname,'public'),
       filename: 'bundle.js'
-  
+   
 },
 module : {
   rules: [{
     loader: 'babel-loader',
     test: /\.js$/,
     exclude: /node_modules/
+  }, {
+    test: /\.s?css/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'sass-loader'
+    ]
+  
   }]
 },
 devtool: 'cheap-module-eval-source-map',
 devServer: {
-  contentBase:path.join(__dirname,'public')
+  contentBase:path.join(__dirname,'public'),
+  historyApiFallback: true
 }
 };
